617ac6309d26447e84c5ec2360bd64e059144b6f
ShamayOsmanova <145850579+ShamayOsmanova@users.noreply.github.com>
2023-12-15 03:00:14 +0400

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      className="footer"
+      style={{
+        color: "#60CCEE",
+        fontSize: "30px",
+        height: "200px",
+        textAlign: "center",
+        padding: "30px",
+      }}
+    >
+      <h1>+(99)1234567</h1>
+      <p>useremail@email.com</p>
+    </div>
+  );
+};
+
+export default Footer;
